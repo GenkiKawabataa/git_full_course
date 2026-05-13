07c69f4726ed9dd0222d69d2086604d5fb0d9c7a
Genki Kawabata <genki_kawabata@nuco.co.jp>
2026-05-13 17:43:55 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 function greet(){
     console.groupCollapsed("Hello from the main branch!");
 }
+=======
+function greet() {
+  console.log("Hello from the local branch!");
+}
+
+>>>>>>> 8530c4c (私たちの修正)
